1ffc9da3419badd3cd4ca1be223c4a1b6714e46e
Kevin Dierkx <contact@kevindierkx.com>
2015-06-06 16:03:47 +0200

    Fixed bug where empty paths would break the inPaths check in the getAttribute method

--- src/Entities/Cloudstack.php
+++ src/Entities/Cloudstack.php
@@ -4,73 +4,68 @@ use Kevindierkx\Elicit\Elicit\Model;
 
 class Cloudstack extends Model {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	protected $connection = 'cloudstack-client';
+    /**
+     * {@inheritDoc}
+     */
+    protected $connection = 'cloudstack-client';
 
-	/**
-	 * {@inheritDoc}
-	 */
-	protected $primaryKey;
+    /**
+     * {@inheritDoc}
+     */
+    protected $primaryKey;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	protected $paths;
+    /**
+     * {@inheritDoc}
+     */
+    protected $defaults = [
+        '*' => [
+            'path'   => '/',
+            'method' => 'GET',
+        ],
 
-	/**
-	 * {@inheritDoc}
-	 */
-	protected $defaults = [
-		'*' => [
-			'path'   => '/',
-			'method' => 'GET',
-		],
+        // The deployment command of a virtual machine supports POST requests.
+        // This way you can send larger amounts of user data.
+        'deployVirtualMachine' => ['method' => 'POST'],
+    ];
 
-		// The deployment command of a virtual machine supports POST requests.
-		// This way you can send larger amounts of user data.
-		'deployVirtualMachine' => ['method' => 'POST'],
-	];
+    /**
+     * Handle dynamic method calls into the method.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        $path = $this->getPath($method);
 
-	/**
-	 * Handle dynamic method calls into the method.
-	 *
-	 * @param  string  $method
-	 * @param  array   $parameters
-	 * @return mixed
-	 */
-	public function __call($method, $parameters)
-	{
-		$path = $this->getPath($method);
+        $queryParameters = array_shift($parameters);
 
-		$queryParameters = array_shift($parameters);
+        // When the path could not be determined form the API list
+        // the command most likely does not exist or is not available
+        // for the user.
+        if ( is_null($path) ) {
+            $className = get_class($this);
 
-		// When the path could not be determined form the API list
-		// the command most likely does not exist or is not available
-		// for the user.
-		if ( is_null($path) ) {
-			$className = get_class($this);
+            throw new \BadMethodCallException("Call to {$method}() is not available in the API list.");
+        }
 
-			throw new \BadMethodCallException("Call to {$method}() is not available in the API list.");
-		}
+        // Here we fetch the query instance bound to the model.
+        // We use this instance to add the path and query parameters
+        // to the request.
+        $query = $this->newQuery();
 
-		// Here we fetch the query instance bound to the model.
-		// We use this instance to add the path and query parameters
-		// to the request.
-		$query = $this->newQuery();
+        $query->from($path);
 
-		$query->from($path);
+        $query->where('command', $method);
 
-		$query->where('command', $method);
+        // We make sure the provided query parameters are of
+        // the type array before adding it to the query.
+        if ( is_array($queryParameters) && ! empty($queryParameters) ) {
+            $query->where($queryParameters);
+        }
 
-		// We make sure the provided query parameters are of
-		// the type array before adding it to the query.
-		if ( is_array($queryParameters) && ! empty($queryParameters) ) {
-			$query->where($queryParameters);
-		}
-
-		return $query->get();
-	}
+        return $query->get();
+    }
 
 }
